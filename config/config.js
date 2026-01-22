e14afb1958a97153d8fcdb1ac7e555aab0a04ccd
Fabio Febre <febrevargasf@gamil.com>
2026-01-22 17:48:25 -0500

    22/1

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: 'base_gkwa_user',
-    password: 'dfrzZENRP6uFTP4nNq8qVuVnkiJKnpDO',
-    database: 'base_gkwa',
-    host: 'dpg-d4tno1adbo4c73aesf60-a.oregon-postgres.render.com',
+    username: 'basesg_user',
+    password: 'mltGRBaN6jC8j9ZKoJnjZcWKwsNjIlMH',
+    database: 'basesg',
+    host: 'dpg-d5p95d3vbchc7390qlr0-a.oregon-postgres.render.com',
     dialect: 'postgres',
     port: 5432,
     dialectOptions: {
