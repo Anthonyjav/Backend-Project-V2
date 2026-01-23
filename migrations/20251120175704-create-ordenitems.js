dde8f35abbc0c63dd2f55a72002acbaf562b8e67
Fabio Febre <febrevargasf@gamil.com>
2026-01-22 19:26:32 -0500

    22/6

--- migrations/20251120175704-create-ordenitems.js
+++ migrations/20251120175704-create-ordenitems.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('ordenitems', {
+    await queryInterface.createTable('OrdenItems', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -57,6 +57,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ordenitems');
+    await queryInterface.dropTable('OrdenItems');
   }
 };
